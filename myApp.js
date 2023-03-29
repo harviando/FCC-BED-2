2b179ec31213535cdcc8ce5f550dba6c3f345031
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-03-29 13:19:04 +0700

    added code to serve strings when user made request on root

--- myApp.js
+++ myApp.js
@@ -3,6 +3,11 @@ let app = express();
 
 console.log("Hello World!");
 
+/** Serve "Hello Express String Whenever User request root or "/" " */
+app.get("/", (req, res)=>{
+    res.send("Hello Express");
+});
+
 
 
 
