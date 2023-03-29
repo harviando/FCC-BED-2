6df72c1d834a76bbdf2ccd66f560aebbcece5b3d
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-03-29 13:31:10 +0700

    adding a response to send a index.html file when requesting root

--- myApp.js
+++ myApp.js
@@ -3,9 +3,15 @@ let app = express();
 
 console.log("Hello World!");
 
-/** Serve "Hello Express String Whenever User request root or "/" " */
-app.get("/", (req, res)=>{
-    res.send("Hello Express");
+// /** Serve "Hello Express String Whenever User request root or "/" " */
+// app.get("/", (req, res)=>{
+//     res.send("Hello Express");
+// });
+
+
+// Responding to a request with sending a index file
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 
